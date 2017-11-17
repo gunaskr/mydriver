a8cf7fa7d4fc30a93fe9f8eb55cf848c9725d887
jmuhammed <jmuhammed@deloitte.com>
2017-11-17 15:55:31 +0530

    Code for file upload.

---
+++ src/main/java/com/lc/df/ftp/sync/controller/FileUpload.java
@@ -0,0 +1,37 @@
+package com.lc.df.ftp.sync.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+
+@RestController
+public class FileUpload {
+
+
+    @Autowired
+    private DefaultSftpSessionFactory defaultSftpSessionFactory;
+
+    @RequestMapping("/uploadFile")
+    public boolean fileUpload() {
+
+        System.out.println("Data:::::::::" + defaultSftpSessionFactory.getSession().isOpen());
+        String localFileFullName = "C:\\\\MyDriver\\\\upload\\\\image.png";
+        System.out.println("File Picked");
+        boolean result = false;
+        try (InputStream input = new FileInputStream(new File(localFileFullName))) {
+
+            //result = this.ftp.storeFile("/pub/examples/" + "image.png", input);
+            defaultSftpSessionFactory.getSession().write(input, "/pub/examples/" + "image.png");
+
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
