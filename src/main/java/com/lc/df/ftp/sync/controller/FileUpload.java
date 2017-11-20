cb7dbb790df7e406537502d4fe7f7fef630c983a
jmuhammed <jmuhammed@deloitte.com>
2017-11-20 17:08:31 +0530

    some changes

--- src/main/java/com/lc/df/ftp/sync/controller/FileUpload.java
+++ src/main/java/com/lc/df/ftp/sync/controller/FileUpload.java
@@ -1,5 +1,6 @@
 package com.lc.df.ftp.sync.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.util.logging.Logger;
 
 @RestController
 public class FileUpload {
@@ -17,10 +19,11 @@ public class FileUpload {
 
     @Value("${ftp.remote.resource.path}")
     private String remotePath;
-
+    private final org.slf4j.Logger LOGGER= LoggerFactory.getLogger(this.getClass());
     @RequestMapping("/uploadFile")
     public boolean fileUpload() {
 
+        LOGGER.info("fileUpload method called");
         System.out.println("Data:::::::::" + defaultSftpSessionFactory.getSession().isOpen());
         String localFileFullName = "C:\\\\ftp_files\\\\pocketftp.png";
         System.out.println("File Picked");
@@ -29,12 +32,16 @@ public class FileUpload {
 
             //result = this.ftp.storeFile("/pub/examples/" + "image.png", input);
             defaultSftpSessionFactory.getSession().write(input, remotePath+ "/image.png");
-
+            LOGGER.info("File Uploaded Successfully");
             return true;
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage());
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error(String.valueOf(e.getStackTrace()));
+        }
+        catch (Exception e)
+        {
+            LOGGER.error(String.valueOf(e.getStackTrace()));
         }
         return false;
     }
