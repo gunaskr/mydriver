00e4df8a1f735f580013847b1e0eea16fc5f2e2e
Jalihal <vjalihal@deloitte.com>
2017-11-23 12:44:24 +0530

    added test for listing the files

---
+++ p2-mydrive-ftpservice/src/test/java/com/lc/df/ftp/sync/controller/FileResourceTest.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.lc.df.ftp.sync.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.lc.df.ftp.sync.service.FileResourceService;
+
+/**
+ * @author vjalihal
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(FileResource.class)
+public class FileResourceTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private FileResourceService fileResourceService;
+
+	@Test
+	public void testGetFileServiceExists() throws Exception {
+
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/getFileList")).andExpect(status().isOk());
+
+	}
+
+}
