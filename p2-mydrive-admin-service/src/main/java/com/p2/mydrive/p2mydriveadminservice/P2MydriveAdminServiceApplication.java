720c66de518f80dda77f5b70116d36e2cb94d072
A <guna@deloitte.com>
2017-11-23 12:15:50 +0530

    added admin service

---
+++ p2-mydrive-admin-service/src/main/java/com/p2/mydrive/p2mydriveadminservice/P2MydriveAdminServiceApplication.java
@@ -0,0 +1,12 @@
+package com.p2.mydrive.p2mydriveadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2MydriveAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2MydriveAdminServiceApplication.class, args);
+	}
+}
